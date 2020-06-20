a6ff8d944197542ee9077995814c1ee1b6645f18
Birch-san <Birch-san@users.noreply.github.com>
2020-06-20 22:47:08 +0100

    remove STRING_SIZE in favour of strlen_. this enables longer printf() formats, longer time() formats, and longer probe names.

--- src/bpftrace.cpp
+++ src/bpftrace.cpp
@@ -545,7 +545,7 @@ void perf_event_printer(void *cb_cookie, void *data, int size __attribute__((unu
   }
   else if (printf_id == asyncactionint(AsyncAction::time))
   {
-    char timestr[STRING_SIZE];
+    char timestr[bpftrace->strlen_];
     time_t t;
     struct tm tmp;
     t = time(NULL);
@@ -797,7 +797,8 @@ std::vector<std::unique_ptr<AttachedProbe>> BPFtrace::attach_usdt_probe(
 
   if (!(file_activation && probe.path.size()))
   {
-    ret.emplace_back(std::make_unique<AttachedProbe>(probe, func, pid));
+    ret.emplace_back(
+        std::make_unique<AttachedProbe>(probe, func, strlen_, pid));
     return ret;
   }
 
@@ -853,7 +854,7 @@ std::vector<std::unique_ptr<AttachedProbe>> BPFtrace::attach_usdt_probe(
       }
 
       ret.emplace_back(
-          std::make_unique<AttachedProbe>(probe, func, pid_parsed));
+          std::make_unique<AttachedProbe>(probe, func, strlen_, pid_parsed));
       break;
     }
   }
@@ -905,13 +906,13 @@ std::vector<std::unique_ptr<AttachedProbe>> BPFtrace::attach_probe(
     else if (probe.type == ProbeType::watchpoint)
     {
       ret.emplace_back(
-          std::make_unique<AttachedProbe>(probe, func->second, pid));
+          std::make_unique<AttachedProbe>(probe, func->second, strlen_, pid));
       return ret;
     }
     else
     {
-      ret.emplace_back(
-          std::make_unique<AttachedProbe>(probe, func->second, safe_mode_));
+      ret.emplace_back(std::make_unique<AttachedProbe>(
+          probe, func->second, strlen_, safe_mode_));
       return ret;
     }
   }
